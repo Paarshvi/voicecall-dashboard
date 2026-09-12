4d504dc86c929fb8333c344a6eddcd4100df46f8
Paarshvi <paarshvi20@gmail.com>
2026-09-12 20:53:53 +0530

    Add call creation endpoint

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Call(BaseModel):
+    customer_name: str
+    phone: str
 
 
 @app.get("/")
 def home():
     return {"message": "VoiceCall Dashboard API is running"}
+
+
+@app.get("/calls")
+def get_calls():
+    return {"calls": []}
+
+
+@app.post("/calls")
+def create_call(call: Call):
+    return {
+        "message": "Call created",
+        "call": call
+    }
